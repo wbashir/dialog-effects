0011e01c30030a8c004d14ae742e6c075e9699c7
Warsame Bashir <warsameb0@gmail.com>
2014-12-21 00:16:38 -0600

    added repeateble .version file, updated package.js to export and inclue main dialog style

--- package.js
+++ package.js
@@ -1,17 +1,23 @@
 Package.describe({
   name: 'bixin:dialogs',
-  summary: ' /* Fill me in! */ ',
+  summary: 'Meteor Codrops Dialog-Effects',
   version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  git: 'https://github.com/warsamebashir/dialog-effects.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2');
-  api.addFiles('bixin:dialogs.js');
+  api.add_files([
+    'dialog-annie.css',
+    'dialog-cathy.css',
+    'dialog.css',
+    'modernizr.custom.js',
+    'classie.js',
+    'dialogFx.js',
+    'snap.svg-min.js',
+  ], ['client']);
+
+  api.export(['DialogFx']);
+
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('bixin:dialogs');
-  api.addFiles('bixin:dialogs-tests.js');
-});
